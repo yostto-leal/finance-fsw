74cd41b36eb14e885cb3b718fd800fd59391399a
yostto-leal <lealariosto@gmail.com>
2024-11-05 17:18:30 -0300

    chore add prisma and datahouse setup

---
+++ prisma/migrations/20241105200103_init_db/migration.sql
@@ -0,0 +1,23 @@
+-- CreateEnum
+CREATE TYPE "TransactionType" AS ENUM ('DEPOSIT', 'EXPENSE', 'INVESTMENT');
+
+-- CreateEnum
+CREATE TYPE "TransactionCategory" AS ENUM ('HOUSING', 'TRANSPORTATION', 'FOOD', 'ENTERTAINHEMT', 'HEALTH', 'UTILITY', 'SALARY', 'EDUCATION', 'OTHER');
+
+-- CreateEnum
+CREATE TYPE "TransactionPaymentMethod" AS ENUM ('CREDIT_CARD', 'DEBIT_CARD', 'BANK_TRANSFER', 'BANK_SLIP', 'CASH', 'PIX', 'OTHER');
+
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "type" "TransactionType" NOT NULL,
+    "amaount" DECIMAL(10,2) NOT NULL,
+    "category" "TransactionCategory" NOT NULL,
+    "paymentMethod" "TransactionPaymentMethod" NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
+);
